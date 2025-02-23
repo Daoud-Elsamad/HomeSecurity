d77900086a255d1c25e16c39d1879ef7c7b4dca2
IyadHajjaj <iyad.alhajjaj@bahcesehir.edu.tr>
2025-02-23 21:14:18 +0300

    Updated the UI in the dashboard.

--- app/src/main/java/com/example/homesecurity/HomeSecurityApp.kt
+++ app/src/main/java/com/example/homesecurity/HomeSecurityApp.kt
@@ -4,4 +4,6 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HomeSecurityApp : Application() 
+class HomeSecurityApp : Application()
+
+//done//
